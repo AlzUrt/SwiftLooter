eb3c4a25ec768170732e22966644fe9d006546be
MAZEL-Florian <mazel.florian1@gmail.com>
2024-10-08 16:35:11 +0200

    first commit

---
+++ swiftApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  swiftApp
+//
+//  Created by MAZEL Florian on 08/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Loot").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+
+        }
+
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftApp/swiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftAppApp.swift
+//  swiftApp
+//
+//  Created by MAZEL Florian on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
